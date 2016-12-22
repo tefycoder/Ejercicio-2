ad7ebe1f842a29704746398ac538902f505ac094
tefy <tefysud21@gmail.com>
2016-12-22 00:07:34 -0500

    js y seudocódigo

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Ejercicio-2
+Calcular promedio de tres números
+
+1. INICIO
+2. Ingresar primer numero
+3. Ingresar segundo numero
+4. Ingresar tercer numero
+5. Calcular el promedio de los tres numeros
+6. Imprimir el promedio
+7. FIN
---
+++ ejercicio2.js
@@ -0,0 +1,6 @@
+var num1 = prompt("Ingresa primer numero");
+var num2 = prompt("Ingresa segundo numero");
+var num3 = prompt("Ingresa tercer numero");
+var prom = (parseInt(num1)+parseInt(num2)+ parseInt(num3))/3;
+document.write("El promedio es: ");
+document.write(prom);
